3769d4fb894da779fd6f22fe30a1890e975cba10
Tim Rottmann <tim.rottmann@gmail.com>
2017-10-02 17:30:24 +0200

    frontend setup

---
+++ frontend/dist/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+
+    <link rel="stylesheet" href="main.css">
+</head>
+<body class="body">
+
+<div class="app" id="app">
+
+
+
+</div>
+
+<script src="main.js"></script>
+</body>
+</html>
--- scrape.php
+++ scrape.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * @param $urls
  * @return array
